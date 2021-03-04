51ca89938403821b5186cdec5fb59a630ccf6a23
VideoGames-SUON <80063825+VideoGames-SUON@users.noreply.github.com>
2021-03-04 16:54:30 -0500

    yifei.js

---
+++ gp/yifei.js
@@ -0,0 +1,35 @@
+
+//Yifei's code//
+
+//title image//
+function surp() {
+	document.getElementById('nani').src= "https://metro.co.uk/wp-content/uploads/2019/02/rainbow-six-siege-new-dcc9.jpg?quality=90&strip=all&crop=0px%2C17px%2C1080px%2C567px&resize=1200%2C630"
+}
+
+function uiu(){
+	document.getElementById('nani').src="https://cdn1.epicgames.com/undefined/offer/R6S_Logo_Color_1000x375-1000x375-8e2b21c06400f36fae8d6a6695cd7155.png?h=854&resize=1&w=640"
+}
+
+//gif.//
+function ash(){
+	document.getElementById('12').src = "https://i.imgur.com/Dnb6XXB.jpg"
+}
+
+//operators image//
+function midori() {
+	document.getElementById('kowaii').src ="https://thumbs.gfycat.com/IncompatibleZealousAlbino-max-1mb.gif"
+} 
+
+//Hibana//
+function hibana(){
+	document.getElementById('hiban').src="https://thumbs.gfycat.com/AffectionateFearfulAidi-size_restricted.gif"
+}
+
+function not(){
+	document.getElementById('hiban').src="https://i.pinimg.com/originals/6c/a5/14/6ca514da4118659c6fc102757c76c2c1.gif"
+}
+
+//End of Yifei's code//
+
+
+
